e660ffc76b03cddcb00750627090d22ebf0f8e01
Zaur Giyasov <zg@numbuster.com>
2025-03-14 11:36:12 +0300

    Initial Commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Zaur  on 14.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
